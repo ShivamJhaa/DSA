415948193b3d5707fa665c239340630addbd5ea8
ShivamJhaa <20bcs206@iiitdmj.ac.in>
2022-02-01 18:42:36 +0530

    Day 3

--- SDE_Sheet/arrays/duplicateNumber.cpp
+++ SDE_Sheet/arrays/duplicateNumber.cpp
@@ -20,3 +20,22 @@ public:
 
 //Without modifying the array in 0(1) space
 
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow != fast);
+        fast = nums[0];
+        while(slow!=fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+        
+    }
+};
